28420a7d762fd062829a8e839da48daf42391577
Monkey <verano@korea.com>
2016-08-29 17:36:52 +0900

    8.29.5:36

--- app/src/main/java/postme/tacademy/com/postme/ATActivity.java
+++ app/src/main/java/postme/tacademy/com/postme/ATActivity.java
@@ -1,12 +1,17 @@
 package postme.tacademy.com.postme;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -18,8 +23,30 @@ public class ATActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.a_at);
 
-        Button btn_join = (Button)findViewById(R.id.btn_join);
-        btn_join.setOnClickListener(new View.OnClickListener() {
+        final Button btn = (Button)findViewById(R.id.btn_join);
+        btn.setEnabled(false);
+        final CheckBox yak_checkbox01 = (CheckBox)findViewById(R.id.yak_checkBox01);
+        final CheckBox yak_checkbox02 = (CheckBox)findViewById(R.id.yak_checkBox02);
+        final CheckBox yak_checkbox03 = (CheckBox)findViewById(R.id.yak_checkBox03);
+        final CheckBox yak_checkbox04 = (CheckBox)findViewById(R.id.yak_checkBox04);
+        CompoundButton.OnCheckedChangeListener checker = new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (yak_checkbox01.isChecked() && yak_checkbox02.isChecked()
+                        && yak_checkbox03.isChecked() && yak_checkbox04.isChecked()) {
+                    btn.setEnabled(true);
+                    btn.setText("Thanks Checked");
+                } else if (btn.isEnabled()) {
+                    btn.setEnabled(false);
+                }
+            }
+        };
+        yak_checkbox01.setOnCheckedChangeListener(checker);
+        yak_checkbox02.setOnCheckedChangeListener(checker);
+        yak_checkbox03.setOnCheckedChangeListener(checker);
+        yak_checkbox04.setOnCheckedChangeListener(checker);
+
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(), "실행", Toast.LENGTH_SHORT).show();
@@ -27,7 +54,13 @@ public class ATActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        //약관동의 Detail
+        findViewById(R.id.yak_view01).setOnClickListener(DetailLestener);
+        findViewById(R.id.yak_view02).setOnClickListener(DetailLestener);
+        findViewById(R.id.yak_view03).setOnClickListener(DetailLestener);
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -42,6 +75,44 @@ public class ATActivity extends AppCompatActivity {
         }
     }
 
+    //약관동의 Detail
+    TextView.OnClickListener DetailLestener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.yak_view01:
+                    new AlertDialog.Builder(ATActivity.this)
+                            .setTitle("얼럿팝업")
+                            .setMessage("약관내용\n\nText")
+                            .setNeutralButton("닫기", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dlg, int sumthin) {
+                                }
+                            })
+                            .show(); //팝업창 보여줌
+                    break;
+                case R.id.yak_view02:
+                    new AlertDialog.Builder(ATActivity.this)
+                            .setTitle("얼럿팝업")
+                            .setMessage("약관내용\n\nText")
+                            .setNeutralButton("닫기", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dlg, int sumthin) {
+                                }
+                            })
+                            .show(); //팝업창 보여줌
+                    break;
+                case R.id.yak_view03:
+                    new AlertDialog.Builder(ATActivity.this)
+                            .setTitle("얼럿팝업")
+                            .setMessage("약관내용\n\nText")
+                            .setNeutralButton("닫기", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dlg, int sumthin) {
+                                }
+                            })
+                            .show(); //팝업창 보여줌
+                    break;
+            }
+        }
+    };
 }
 
 
--- app/src/main/java/postme/tacademy/com/postme/UserInfoActivity.java
+++ app/src/main/java/postme/tacademy/com/postme/UserInfoActivity.java
@@ -4,8 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -16,6 +22,27 @@ public class UserInfoActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.a_userinfo);
+
+        //Spinner
+        Spinner spinner;
+        spinner = (Spinner)findViewById(R.id.spinner_uinfo);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getApplicationContext(),
+                R.array.nacimiento_array, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                Log.i("Tag","index" + position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        
+        //Bottom Button
         Button btn_userinfo = (Button)findViewById(R.id.btn_userinfo);
         btn_userinfo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,5 +52,23 @@ public class UserInfoActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        
+        //Radio Select
+        findViewById(R.id.radio_man_uinfo).setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View view) {
+                printChecked(view);
+            }
+        });
     }
+
+    private void printChecked(View view) {
+        RadioButton radiouinfo = (RadioButton)view;
+        String resultText = "";
+        if (radiouinfo.isChecked()) {
+            resultText = radiouinfo.getText().toString();
+        }
+        TextView tv = (TextView)findViewById(R.id.text_uinfo_select);
+        tv.setText(resultText);
+    }
+
 }
