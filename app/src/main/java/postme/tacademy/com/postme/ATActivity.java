d67ca08ea26243cc1e393633e75182ec9ad5be79
Monkey <verano@korea.com>
2016-08-29 10:38:08 +0900

    Intro

---
+++ app/src/main/java/postme/tacademy/com/postme/ATActivity.java
@@ -0,0 +1,47 @@
+package postme.tacademy.com.postme;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by Monkey on 2016. 8. 29..
+ */
+public class ATActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_at);
+
+        Button btn_join = (Button)findViewById(R.id.btn_join);
+        btn_join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "실행", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(ATActivity.this, UserInfoActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
+
+
--- app/src/main/java/postme/tacademy/com/postme/UserInfoActivity.java
+++ app/src/main/java/postme/tacademy/com/postme/UserInfoActivity.java
@@ -1,6 +1,5 @@
 package postme.tacademy.com.postme;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -9,19 +8,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by Monkey on 2016. 8. 29..
+ */
+public class UserInfoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button login_btn = (Button)findViewById(R.id.login);
-        login_btn.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.a_userinfo);
+        Button btn_userinfo = (Button)findViewById(R.id.btn_userinfo);
+        btn_userinfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(), "실행", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(MainActivity.this, ATActivity.class);
+                Intent intent = new Intent(UserInfoActivity.this, TabActivity.class);
                 startActivity(intent);
-                finish();
             }
         });
     }
