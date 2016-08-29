49b13ca9d751f2ecaf8238447db1570c59857cb3
wonhochoi <dnjsghek2@naver.com>
2016-08-29 17:41:57 +0900

    2016.08.29 PM 5:40
    마커, 위치 권한등등 작업중

--- app/src/main/java/postme/tacademy/com/postme/Adapter/CH_Pick_Rc_Adapter.java
+++ app/src/main/java/postme/tacademy/com/postme/Adapter/CH_Pick_Rc_Adapter.java
@@ -1,11 +1,16 @@
 package postme.tacademy.com.postme.Adapter;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import java.util.List;
 
@@ -16,15 +21,13 @@ import postme.tacademy.com.postme.R;
 /**
  * Created by wonhochoi on 2016. 8. 24..
  */
-public class CH_Pick_Rc_Adapter extends RecyclerView.Adapter<CH_Pick_Rc_Adapter.ViewHolder>{
+public class CH_Pick_Rc_Adapter extends RecyclerView.Adapter<CH_Pick_Rc_Adapter.ViewHolder> {
     private List<String> cards;
     private OnItemTouchListener onItemTouchListener;
 
-
-    public CH_Pick_Rc_Adapter(List<String> cards, OnItemTouchListener onItemTouchListener) {
+    public CH_Pick_Rc_Adapter(List<String> cards, OnItemTouchListener onItemTouchListener, Fragment getFragment) {
         this.cards = cards;
         this.onItemTouchListener = onItemTouchListener;
-
     }
 
     @Override
@@ -39,20 +42,35 @@ public class CH_Pick_Rc_Adapter extends RecyclerView.Adapter<CH_Pick_Rc_Adapter.
         viewHolder.title.setText(cards.get(i));
 
     }
+
     @Override
     public int getItemCount() {
         return cards == null ? 0 : cards.size();
     }
 
+
     public class ViewHolder extends RecyclerView.ViewHolder {
         private TextView title;
         private Button button2;
+        private ToggleButton pick_local_btn;
+        private ImageView pick_local_view;
 
         public ViewHolder(View itemView) {
             super(itemView);
             title = (TextView) itemView.findViewById(R.id.card_view_title);
             button2 = (Button) itemView.findViewById(R.id.card_view_button2);
-
+            pick_local_btn = (ToggleButton) itemView.findViewById(R.id.Pick_local_btn);
+            pick_local_view = (ImageView) itemView.findViewById(R.id.Pick_local_view);
+            pick_local_btn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    if (b) {
+                        pick_local_view.setVisibility(View.VISIBLE);
+                    } else {
+                        pick_local_view.setVisibility(View.GONE);
+                    }
+                }
+            });
             button2.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
--- app/src/main/java/postme/tacademy/com/postme/Fragment/MapFragment.java
+++ app/src/main/java/postme/tacademy/com/postme/Fragment/MapFragment.java
@@ -16,11 +16,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.Observer;
 
@@ -42,6 +44,9 @@ public class MapFragment extends Fragment implements
     static public MenuInflater menuInflater;
     static View view;
     static public FloatingActionButton fab;
+    static public SupportMapFragment fragment;
+    static final LatLng SEOUL = new LatLng(37.56, 126.97);
+    private GoogleMap googleMap;
     public MapFragment() {
     }
 
@@ -50,7 +55,7 @@ public class MapFragment extends Fragment implements
                              Bundle savedInstanceState) {
         if (view == null) {
             view = inflater.inflate(R.layout.f_map, container, false); //비어있는 layout 생성
-            SupportMapFragment fragment =
+            fragment =
                     (SupportMapFragment) this.getChildFragmentManager()
                             .findFragmentById(R.id.map_fragment);
             fragment.getMapAsync(this);
@@ -116,9 +121,33 @@ public class MapFragment extends Fragment implements
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(GoogleMap Map) {
+        googleMap = Map;
 
+
+        if (ActivityCompat.checkSelfPermission(getContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getContext(),
+                android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
+
+        Marker seoul = googleMap.addMarker(new MarkerOptions().position(SEOUL)
+                .title("Seoul"));
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom( SEOUL, 15));
+
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
     }
+
 
     @Override
     public boolean onMarkerClick(Marker marker) {
